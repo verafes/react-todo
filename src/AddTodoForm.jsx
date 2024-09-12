c7be391849ae3573acdd5244320f813916d1ce0a
verafes <verafes@gmail.com>
2024-09-12 16:29:36 -0700

    Lesson 3.1

--- src/AddTodoForm.jsx
+++ src/AddTodoForm.jsx
@@ -1,12 +1,13 @@
 import {useState} from 'react';
 import InputWithLabel from './InputWithLabel';
+import style from "./AddTodoForm.module.css";
 
 function AddTodoForm({ onAddTodo }) {
     // New state variable and function to handle title change
     const [todoTitle, setTodoTitle] = useState("");
 
     function handleTitleChange(event) {
-        console.log(todoTitle);
+        // console.log(todoTitle);
         let newTodoTitle = event.target.value;
         setTodoTitle(newTodoTitle);
     }
@@ -18,19 +19,19 @@ function AddTodoForm({ onAddTodo }) {
         setTodoTitle("");
     };
     return (
-        <form onSubmit={handleAddTodo}>
-            <InputWithLabel
-                id="todoTitle"
-                type="text"
-                name="title"
-                value={todoTitle}
-                onChange={handleTitleChange}
-            >
-                <strong>Title: </strong>
-            </InputWithLabel>
-            <button type="submit">Add</button>
+        <form onSubmit={handleAddTodo} className={style.form}>
+                <InputWithLabel
+                    id="todoTitle"
+                    type="text"
+                    name="title"
+                    value={todoTitle}
+                    onChange={handleTitleChange}
+                >
+                    <strong>Title: </strong>
+                </InputWithLabel>
+                <button className={style.submitButton} type="submit">Add</button>
         </form>
-    );
+);
 }
 
 export default AddTodoForm;
--- src/TodoListItem.jsx
+++ src/TodoListItem.jsx
@@ -1,14 +1,19 @@
+import style from './TodoListItem.module.css';
+
 // Component to display a single todo item and remove item button
 function TodoListItem({ todo, onRemoveTodo }) {
     return (
-        <li>
-            {todo.title}
-            <button
-                type="button"
-                onClick={ () => onRemoveTodo(todo.id) }
-            >
-                Remove
-            </button>
+        <li className={style.listItem}>
+            <div className={style.listItemContent}>
+                <span className={style.todoText}>{todo.title}</span>
+                <button
+                    className={style.removeButton}
+                    type="button"
+                    onClick={() => onRemoveTodo(todo.id)}
+                >
+                    Remove
+                </button>
+            </div>
         </li>
     );
 }
