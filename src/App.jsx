a570ea63d8dfa1d2851df809647a846c6fb8e584
verafes <verafes@gmail.com>
2024-10-05 23:23:07 -0700

    refactor navbar: implement hamburger menu and narrow mobile navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import './App.css'
-import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import TodoContainer from './components/TodoContainer.jsx'
 import NavBar from './components/NavBar.jsx';
-import {apiKey, baseId, tableName, tableName1} from '../config.js';
+import {apiKey, baseId, tableName} from '../config.js';
 import listIcon from "./img/list.png";
 import style from "./components/TodoContainer.module.css";
 
@@ -22,11 +22,10 @@ function App() {
                 }/>
                 <Route exact path="/mylists" element={
                     <TodoContainer
-                            tableName={tableName1}
+                            tableName={tableName}
                             baseId={baseId}
                             apiKey={apiKey}/>
                 }/>
-                <Route path="/mylists" element={<h1>My Lists</h1>}/>
                 <Route path="/new" element={
                     <div className={style.container}>
                         <img src={listIcon} alt="List Icon" className={style.icon}/>
@@ -34,8 +33,8 @@ function App() {
                     </div>
                 }/>
             </Routes>
-            </BrowserRouter>
-            )
-        }
+        </BrowserRouter>
+    )
+}
 
-                    export default App;
+export default App;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,18 +1,42 @@
 import {Link} from 'react-router-dom';
+import { useState } from 'react';
 import style from './NavBar.module.css';
+import HamburgerIcon from '../img/hamburger-menu.svg';
 
 function NavBar() {
+    const [isOpen, setIsOpen] = useState(false);
+
+    // Toggle the menu state
+    const toggleMenu = () => {
+        setIsOpen(prevState => !prevState);
+    };
+
+    // Collapse the menu when a link is clicked
+    const handleLinkClick = () => {
+        setIsOpen(false);
+    };
+
+    // Check if the screen width is mobile size
+    const isMobile = window.innerWidth <= 768;
 
     return (
         <nav className={style.navBar}>
-            <ul className={style.navList}>
-                <li className={style.navItem}>
+            {isMobile && (
+                <img
+                    src={HamburgerIcon}
+                    alt="Menu"
+                    className={style.hamburger}
+                    onClick={toggleMenu}
+                />
+            )}
+            <ul className={`${style.navList} ${isOpen ? style.open : ''}`}>
+                <li className={style.navItem} onClick={handleLinkClick}>
                     <Link to="/">Home</Link>
                 </li>
-                <li className={style.navItem}>
+                <li className={style.navItem} onClick={handleLinkClick}>
                     <Link to="/mylists">My lists</Link>
                 </li>
-                <li className={style.navItem}>
+                <li className={style.navItem} onClick={handleLinkClick}>
                     <Link to="/new">New Todo List</Link>
                 </li>
             </ul>
