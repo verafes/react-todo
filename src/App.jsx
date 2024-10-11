545dec709f63ecb902d1f9d571a34f4ee3354116
verafes <verafes@gmail.com>
2024-10-10 20:17:04 -0700

    Add footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css'
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import {apiKey, baseId, tableName} from '../config.js';
 import TodoContainer from './components/TodoContainer.jsx'
 import NavBar from './components/NavBar.jsx';
 import HomePage from './pages/HomePage';
 import NewListPage from './pages/NewListPage';
 import NotFoundPage from './pages/NotFoundPage';
-import {apiKey, baseId, tableName} from '../config.js';
 import TodoListSelector from './components/TodoListSelector';
+import Footer from './components/Footer';
 
 
 function App() {
@@ -30,6 +31,7 @@ function App() {
                 }/>
                 <Route path="/new" element={<NewListPage />} />
             </Routes>
+            <Footer />
         </BrowserRouter>
     )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+function Footer() {
+    return (
+        <footer style={{
+            backgroundColor: '#f1f1f1',
+            // padding: '0.25rem',
+            textAlign: 'center',
+            position: 'fixed',
+            left: '0',
+            bottom: '0',
+            width: '100%',
+        }}>
+            <p style={{ fontSize: '0.8em' }}>© 2024 My ToDo App. All rights reserved.</p>
+        </footer>
+    );
+}
+
+export default Footer;
